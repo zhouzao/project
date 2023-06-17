e1e5fef6fcf8548315fbc1420ac4e7589637f8b9
zz <2491918581@qq.com>
2023-06-17 09:41:51 +0800

    测试功能

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import jQuery from "jquery";
+import _ from "lodash";
+import { a } from "@/script/app";
+import "@/style/index.less";
+import "@/index.css"
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log('qe')
+jQuery(function () {
+  console.log(123);
+});
+jQuery.ajax({
+  url: "http://127.0.0.1:3000/api/abc",
+});
+jQuery.ajax({
+  url: "http://127.0.0.1:3000/api1/abc1",
+});
+console.log(_);
+console.log("测试gitee---github同步功能")
+console.log("这是一个index.js")
